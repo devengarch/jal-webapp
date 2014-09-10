23e8d1b03071f5c9146c7ed84f94f9b47b7162d2
devengarch <devengarch@users.noreply.github.com>
2014-09-09 20:38:25 -0600

    Update userdata.ps1

--- userdata.ps1
+++ userdata.ps1
@@ -20,7 +20,7 @@ Install-WindowsFeature as-web-support -IncludeManagementTools
 Import-Module AWSPowerShell
 New-Item -Type Directory 'c:\tmp'
 New-Item -Type Directory 'c:\ImageGallery'
-Read-S3Object -BucketName jalwebapp -Key 'code/jal-webapp-dr.zip' -File 'c:\tmp\jal-webapp-master.zip'
+Read-S3Object -BucketName jalwebapp -Key 'code/jal-webapp-dr.zip' -File 'c:\tmp\jal-webapp-dr.zip'
 
 # Cleanup IIS of existing default and webapp web sites
 Import-Module WebAdministration
@@ -32,11 +32,11 @@ if(("IIS:\AppPools\" + "$WebSiteName")){Remove-WebAppPool $WebSiteName}
 
 # Unzip downloaded webapp code and move to c:\ImageGallery
 $shell_app=new-object -com shell.application
-$zip_file = $shell_app.namespace("c:\tmp\jal-webapp-master.zip")
+$zip_file = $shell_app.namespace("c:\tmp\jal-webapp-dr.zip")
 $destination = $shell_app.namespace("c:\tmp\")
 $destination.Copyhere($zip_file.items())
 if(Test-Path c:\ImageGallery){Remove-Item C:\ImageGallery\* -Recurse -Force}
-Move-Item C:\tmp\jal-webapp-master\* C:\ImageGallery\
+Move-Item C:\tmp\jal-webapp-dr\* C:\ImageGallery\
 Remove-Item c:\tmp\*
 
 # Create new web site using C:\ImageGallery as the root
